0f2ff239b6e59a970f994c4b615a4a97721ed133
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-01-15 15:08:36 -0800

    debugged and implemented tests after changing around some configuration

--- src/bookmarks/bookmarks-service.js
+++ src/bookmarks/bookmarks-service.js
@@ -1,9 +1,15 @@
 const BookmarksService = {
   getAllBookmarks(knex) {
-    return knex.select('*').from('bookmarks')
+    return knex
+      .select('*')
+      .from('bookmarks');
   },
   getById(knex, id) {
-    return knex.from('bookmarks').select('*').where('id', id).first()
+    return knex
+      .from('bookmarks')
+      .select('*')
+      .where('id', id)
+      .first();
   },
   insertBookmark(knex, newBookmark) {
     return knex
@@ -11,19 +17,23 @@ const BookmarksService = {
       .into('bookmarks')
       .returning('*')
       .then(rows => {
-        return rows[0]
-      })
+        return rows[0];
+      });
   },
   deleteBookmark(knex, id) {
     return knex('bookmarks')
       .where({ id })
-      .delete()
+      .delete();
   },
-  updateBookmark(knex, id, newBookmarkFields) {
+  updateBookmark(
+    knex,
+    id,
+    newBookmarkFields
+  ) {
     return knex('bookmarks')
       .where({ id })
-      .update(newBookmarkFields)
-  },
-}
+      .update(newBookmarkFields);
+  }
+};
 
-module.exports = BookmarksService
+module.exports = BookmarksService;
--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/bookmarks',
-}
+  NODE_ENV:
+    process.env.NODE_ENV ||
+    'development',
+  API_TOKEN:
+    process.env.API_TOKEN ||
+    '910237e9-95fd-4ecf-b17b-4af6605a1f01',
+  DB_URL:
+    process.env.DATABASE_URL ||
+    'postgresql://dunder_mifflin@localhost/bookmarks'
+};
